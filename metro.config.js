2aa4256b3420e8513cd9f6682c429e255a118160
fullstackdev2010 <fullstackdev2010@gmail.com>
2025-11-28 10:32:46 +0300

    apple production 15

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: true,
+});
 const path = require('path');
 
 module.exports = withNativeWind(config, { input: "./global.css" });
