8538a74b6c948011e91b77547777fda891db4a07
fullstackdev2010 <fullstackdev2010@gmail.com>
2025-11-29 13:32:10 +0300

    release 15

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 // lib/constants.ts
 //export const API_HOST = 'http://127.0.0.1:8000';
-//export const API_HOST = 'https://api.iskrasales.ru';
-export const API_HOST = 'https://api.iskrasales.online';
+export const API_HOST = 'https://api.iskrasales.ru';
+//export const API_HOST = 'https://api.iskrasales.online';
